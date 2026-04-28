60fd8377a211db64d8d30b1c0178bc285566d8a7
Keinwind <KeinwindEichenSchwert@proton.me>
2026-04-28 12:01:49 +0200

    created proj + cookie.png and counter

---
+++ CookieClicker/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CookieClicker;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    private int _cookie = 0;
+    public int Cookie 
+    { 
+        get => _cookie; 
+        set { _cookie = value; OnPropertyChanged(); } 
+    }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this; // Required to link XAML to this class
+    }
+
+    private void OnCounterClicked(object? sender, EventArgs e)
+    {
+        Cookie++;
+        SemanticScreenReader.Announce($"Clicked {Cookie} times");
+    }
+
+    public new event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
