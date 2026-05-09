10477c676e205177441a45f55f5e3ecf7451cb57
Keinwind <KeinwindEichenSchwert@proton.me>
2026-05-09 17:44:54 +0200

    addad more buy

--- CookieClicker/MainPage.xaml.cs
+++ CookieClicker/MainPage.xaml.cs
@@ -25,8 +25,33 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
     }
     //  Upgrades
     private void OnBuyClicker(object? sender, EventArgs e) {
-        if (Cookie > 0) {
+        if (Cookie >= 1) {
             Cookie--;
+        }
+        
+        SemanticScreenReader.Announce($"Clicked {Cookie} times");
+    }
+    
+    private void OnBuyMelon(object? sender, EventArgs e) {
+        if (Cookie >= 5) {
+            Cookie = Cookie -5;
+        }
+        
+        SemanticScreenReader.Announce($"Clicked {Cookie} times");
+    }
+    
+    private void OnBuyTable(object? sender, EventArgs e) {
+        if (Cookie >= 10) {
+            Cookie = Cookie -10;
+
+        }
+        
+        SemanticScreenReader.Announce($"Clicked {Cookie} times");
+    }
+    
+    private void OnBuyStand(object? sender, EventArgs e) {
+        if (Cookie >= 20) {
+            Cookie = Cookie -20;
         }
         
         SemanticScreenReader.Announce($"Clicked {Cookie} times");
